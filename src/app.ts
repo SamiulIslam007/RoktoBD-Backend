53244f6511235c4dd7034cd7b17dcf4d4a7e7a36
Samiul Islam <samiul10203040@gmail.com>
2026-03-19 01:43:47 +0600

    feat: add auth routes, cookie parser and API base setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import routes from './routes';
+import { globalErrorHandler } from './middlewares/globalErrorHandler';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Apollo Gears World!');
+  res.send('RoktoBd API');
 });
+
+app.use('/api/v1', routes);
+
+app.use(globalErrorHandler);
 
 export default app;
